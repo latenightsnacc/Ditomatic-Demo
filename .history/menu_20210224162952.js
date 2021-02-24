deceeda3cb7866f4758eeccff617bf066be47475
Deborah <54311514+latenightsnacc@users.noreply.github.com>
2021-02-24 16:49:25 -0500

    made the side menu responsive

--- .history/menu_20210224162952.js
+++ .history/menu_20210224162952.js
@@ -1,4 +1,4 @@
-let mainContent = document.getElementById('content');
+let portraitGallery = document.getElementById('portraits');
 let sideMenu = document.getElementById('menu');
 let themeBg = document.getElementById('main-wrapper');
 
@@ -11,16 +11,7 @@ document.getElementById('themeBtn').addEventListener('click', function(){
         themeBg.classList.replace('bg-gray-900', 'bg-gray-100');
     }
 } );
-
 document.getElementById('menuBtn').addEventListener('click', function(){
     
-    if(sideMenu.style.display != 'none'){
-        sideMenu.style.display = 'none';
-        mainContent.classList.remove('ml-60');
-        mainContent.classList.replace('w-11/12', 'w-full');
-    } else {
-        sideMenu.style.display = 'block';
-        mainContent.classList.replace('w-full', 'w-11/12');
-        mainContent.classList.add('ml-60');
-    }
+    alert('clicked');
 } );
--- .history/menu_20210224164039.js
+++ .history/menu_20210224164039.js
@@ -11,16 +11,13 @@ document.getElementById('themeBtn').addEventListener('click', function(){
         themeBg.classList.replace('bg-gray-900', 'bg-gray-100');
     }
 } );
-
 document.getElementById('menuBtn').addEventListener('click', function(){
     
     if(sideMenu.style.display != 'none'){
         sideMenu.style.display = 'none';
-        mainContent.classList.remove('ml-60');
-        mainContent.classList.replace('w-11/12', 'w-full');
+        mainContent.classList.replace('ml-60', 'ml-20');
     } else {
         sideMenu.style.display = 'block';
-        mainContent.classList.replace('w-full', 'w-11/12');
-        mainContent.classList.add('ml-60');
+        mainContent.classList.replace('ml-20', 'ml-60');
     }
 } );
